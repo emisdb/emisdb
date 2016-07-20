d078d28e950a48c3c856051fa8a8266217849582
Denis <dentsi@yahoo.com>
2016-07-20 16:34:26 +0300

    gii client model

---
+++ common/models/Client.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "client".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property integer $country_id
+ */
+class Client extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'client';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'country_id'], 'required'],
+            [['country_id'], 'integer'],
+            [['name'], 'string', 'max' => 64]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'country_id' => 'Country ID',
+        ];
+    }
+}
