fd1d37ba7c233ecf9025f20abf92c67aae15ebb1
Denis <dentsi@yahoo.com>
2018-08-16 18:37:31 +0300

    motu experiments

---
+++ frontend/web/motu/js/myjs.js
@@ -0,0 +1,94 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+function plusDivst(n) {
+  showDivst(slideIndext += n);
+}
+
+function currentDivt(n) {
+  showDivst(slideIndext = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+ x[slideIndex-1].style.display = "block";  
+
+}
+function showDivst(n) {
+  var i;
+  var x = document.getElementsByClassName("myTeam");
+  var dots = document.getElementsByClassName("demon");
+  if (n > x.length) {slideIndext = 1}    
+  if (n < 1) {slideIndext = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndext-1].style.display = "block";  
+  dots[slideIndext-1].className += " w3-white";
+}
+function accFunction(id) {
+    var y = document.getElementsByClassName("accordion");
+    var x = document.getElementById(id);
+    for (i = 0; i < y.length; i++) {
+        if(y[i]!=x){  y[i].className = y[i].className.replace(" w3-show", "");}
+  }
+     if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+    }
+}
+
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+
+
+// Toggle between showing and hiding the sidebar when clicking the menu icon
+var mySidebar = document.getElementById("mySidebar");
+
+function w3_open() {
+    if (mySidebar.style.display === 'block') {
+        mySidebar.style.display = 'none';
+    } else {
+        mySidebar.style.display = 'block';
+    }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+}
+
+
