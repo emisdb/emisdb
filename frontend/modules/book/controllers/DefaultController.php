b4b2c58bd8777898e2da5400211d684de20bce36
emisd <emisd@DESKTOP-CLVG4OB>
2020-09-15 10:19:53 +0300

    structure for spb.gov api with pagination

--- frontend/modules/book/controllers/DefaultController.php
+++ frontend/modules/book/controllers/DefaultController.php
@@ -4,6 +4,7 @@ namespace app\modules\book\controllers;
 
 use common\components\ApiDataProvider;
 use yii\data\Pagination;
+use Yii;
 
 /**
  * Default controller for the `book` module
@@ -29,7 +30,21 @@ class DefaultController extends AppController {
 		$heads = $dp->getData($id, 'versions/latest/');
 		$headers = array_column($heads["structure"],'name','title');
 		$pagination = new Pagination(['totalCount' => 100, 'pageSize' => 20]);
-		$data = $dp->getData($id, 'versions/latest/data/?per_page=20&page=1');//.$pagination->getPage());
+		$pNum = Yii::$app->request->get('page');
+		if($pNum === null){
+			$data = $dp->getData($id, 'versions/latest/data/?per_page=20&page=1');
+			$pagination = new Pagination(['totalCount' => 80, 'pageSize' => 20]);		
+		}
+		else
+		{
+			$data = $dp->getData($id, 'versions/latest/data/?per_page=20&page='.$pNum);			
+			if($data === null) {
+				$pagination = new Pagination(['totalCount' => ((int)$pNum)*20, 'pageSize' => 20]);						
+			}
+			else {
+				$pagination = new Pagination(['totalCount' => ((int)$pNum+3)*20, 'pageSize' => 20]);						
+			}
+		}
 		return $this->render('spb_data', [
 			'data'=>$data,
 			'headers'=>$headers,
